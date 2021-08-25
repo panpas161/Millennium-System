d10dab5efd072132d82c71475730c8013b9e1b7f
panpas161 <panpas161@gmail.com>
2021-08-25 18:41:49 +0300

    Fixed issue

--- Millennium_System/roles.py
+++ Millennium_System/roles.py
@@ -7,50 +7,53 @@ from espa.models import SubsidizedBusiness,EspaAssociate
 from associate.models import Associate
 
 #All roles available here
-ROLES = {
-    0: {
-        'Name':'Admin',
-        'Group':Group.objects.get_or_create(name="Administrator")[0],
-        'Model':Administrator,
-        'HomePage':'home'
-    },
-    1: {
-        'Name':'Staff',
-        'Group':Group.objects.get_or_create(name="Staff")[0],
-        'Model':Staff,
-        'HomePage':'home'
-    },
-    2: {
-        'Name':'Student',
-        'Group':Group.objects.get_or_create(name="Student")[0],
-        'Model':Student,
-        'HomePage':'students_front_home'
-    },
-    3: {
-        'Name':'Teacher',
-        'Group':Group.objects.get_or_create(name="Teacher")[0],
-        'Model':Teacher,
-        'HomePage':'teacher_home_page'
-    },
-    4: {
-        'Name':'EspaUser',
-        'Group':Group.objects.get_or_create(name="EspaUser")[0],
-        'Model':SubsidizedBusiness,
-        'HomePage':'espa_home'
-    },
-    5: {
-        'Name':'Associate',
-        'Group':Group.objects.get_or_create(name="Associate")[0],
-        'Model':Associate,
-        'HomePage':'list_interested_businesses'
-    },
-    6: {
-        'Name':'EspaAssociate',
-        'Group':Group.objects.get_or_create(name="EspaAssociate")[0],
-        'Model':EspaAssociate,
-        'HomePage':'list_interested_businesses'
-    },
-}
+try:
+    ROLES = {
+        0: {
+            'Name':'Admin',
+            'Group':Group.objects.get_or_create(name="Administrator")[0],
+            'Model':Administrator,
+            'HomePage':'home'
+        },
+        1: {
+            'Name':'Staff',
+            'Group':Group.objects.get_or_create(name="Staff")[0],
+            'Model':Staff,
+            'HomePage':'home'
+        },
+        2: {
+            'Name':'Student',
+            'Group':Group.objects.get_or_create(name="Student")[0],
+            'Model':Student,
+            'HomePage':'students_front_home'
+        },
+        3: {
+            'Name':'Teacher',
+            'Group':Group.objects.get_or_create(name="Teacher")[0],
+            'Model':Teacher,
+            'HomePage':'teacher_home_page'
+        },
+        4: {
+            'Name':'EspaUser',
+            'Group':Group.objects.get_or_create(name="EspaUser")[0],
+            'Model':SubsidizedBusiness,
+            'HomePage':'espa_home'
+        },
+        5: {
+            'Name':'Associate',
+            'Group':Group.objects.get_or_create(name="Associate")[0],
+            'Model':Associate,
+            'HomePage':'list_interested_businesses'
+        },
+        6: {
+            'Name':'EspaAssociate',
+            'Group':Group.objects.get_or_create(name="EspaAssociate")[0],
+            'Model':EspaAssociate,
+            'HomePage':'list_interested_businesses'
+        },
+    }
+except:
+    pass
 
 #Add admin roles here
 
